14250715e9e088fa28e81d62db72de7274569426
Antoine Aumjaud <antoine_dev@aumjaud.fr>
2017-07-05 18:39:53 +0200

    :bug: fix missing annotation

--- src/main/java/com/tesobe/obp/botclient/InfoRessource.java
+++ src/main/java/com/tesobe/obp/botclient/InfoRessource.java
@@ -5,9 +5,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.tesobe.obp.schedule.ScheduledTasks;
 
+@RestController
 public class InfoRessource {
 
 	private final Logger log = LoggerFactory.getLogger(InfoRessource.class);
