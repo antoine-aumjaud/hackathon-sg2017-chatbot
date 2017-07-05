84ec418702d67ae2fada55fa8166b4cf6c3e7807
wadinj <wadin.jonathan@gmail.com>
2017-07-05 17:57:36 +0200

    [fix] add transaction list endpoint without mocks

--- src/main/java/com/tesobe/obp/account/Transaction.java
+++ src/main/java/com/tesobe/obp/account/Transaction.java
@@ -1,93 +1,97 @@
 package com.tesobe.obp.account;
 
+import java.util.Date;
+import java.util.List;
+
+import org.joda.money.Money;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.tesobe.obp.Application;
 import com.tesobe.obp.MoneyJson;
-import lombok.Builder;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.joda.money.Money;
-
-import java.util.Date;
-import java.util.List;
 
 @Data
 public class Transaction {
 
-    private String id;
+	private String id;
 
-    @JsonProperty("other_account")
-    private Account targetAccount;
+	@JsonProperty("other_account")
+	private Account targetAccount;
 
-    @JsonProperty("this_account")
-    private Account ownAccount;
+	@JsonProperty("this_account")
+	private Account ownAccount;
 
-    private Details details;
+	private Details details;
 
-    private Metadata metadata;
+	private Metadata metadata;
 
-    @Data
-    private class Details {
-        private String type;
-        private String description;
+	@Data
+	public class Details {
+		private String type;
+		private String description;
 
-        @JsonProperty("posted")
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = Application.ISO8601_TIMESTAMP_FORMAT, timezone = "UTC")
-        private Date postedDate;
+		@JsonProperty("posted")
+		@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = Application.ISO8601_TIMESTAMP_FORMAT, timezone = "UTC")
+		private Date postedDate;
 
-        @JsonProperty("completed")
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = Application.ISO8601_TIMESTAMP_FORMAT, timezone = "UTC")
-        private Date completedDate;
+		@JsonProperty("completed")
+		@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = Application.ISO8601_TIMESTAMP_FORMAT, timezone = "UTC")
+		private Date completedDate;
 
-        @JsonProperty("new_balance")
-        @JsonDeserialize(using = MoneyJson.MoneyDeserializer.class)
-        private Money newBalance;
+		@JsonProperty("new_balance")
+		@JsonDeserialize(using = MoneyJson.MoneyDeserializer.class)
+		private Money newBalance;
 
-        @JsonProperty("value")
-        @JsonDeserialize(using = MoneyJson.MoneyDeserializer.class)
-        private Money value;
-    }
+		@JsonProperty("value")
+		@JsonDeserialize(using = MoneyJson.MoneyDeserializer.class)
+		private Money value;
+	}
 
-    @Data
-    public class Metadata {
-        private String narrative;
-        private List<Object> comments;
-        private List<Tag> tags;
-        private List<Object> images;
+	@Data
+	public class Metadata {
+		private String narrative;
+		private List<Object> comments;
+		private List<Tag> tags;
+		private List<Object> images;
 
-        @JsonProperty("where")
-        private Object location;
-    }
+		@JsonProperty("where")
+		private Object location;
+	}
 
-    @Data
-    @NoArgsConstructor
-    public static class Tag {
-        public Tag(String value) {
-            this.value = value;
-        }
-        private String value;
+	@Data
+	@NoArgsConstructor
+	public static class Tag {
+		public Tag(String value) {
+			this.value = value;
+		}
 
-        private String id;
+		private String value;
 
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = Application.ISO8601_TIMESTAMP_FORMAT, timezone = "UTC")
-        @JsonProperty("date")
-        private Date createdAt;
+		private String id;
 
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Tag tag = (Tag) o;
-            return id.equals(tag.id);
-        }
+		@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = Application.ISO8601_TIMESTAMP_FORMAT, timezone = "UTC")
+		@JsonProperty("date")
+		private Date createdAt;
 
-        @Override
-        public int hashCode() {
-            int result = super.hashCode();
-            result = 31 * result + id.hashCode();
-            return result;
-        }
-    }
+		@Override
+		public boolean equals(Object o) {
+			if (this == o)
+				return true;
+			if (o == null || getClass() != o.getClass())
+				return false;
+			Tag tag = (Tag) o;
+			return id.equals(tag.id);
+		}
+
+		@Override
+		public int hashCode() {
+			int result = super.hashCode();
+			result = 31 * result + id.hashCode();
+			return result;
+		}
+	}
 }
