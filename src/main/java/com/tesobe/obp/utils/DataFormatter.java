26dd0a30eb429b5a2459b3c5d023017c5b707547
Antoine Aumjaud <antoine_dev@aumjaud.fr>
2017-07-06 11:06:24 +0200

    :rotating_light: add test

--- src/main/java/com/tesobe/obp/utils/DataFormatter.java
+++ src/main/java/com/tesobe/obp/utils/DataFormatter.java
@@ -2,26 +2,24 @@ package com.tesobe.obp.utils;
 
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
-import org.junit.Test;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DataFormatter {
 
-//	@Test
-	public void main()
-	{
-//	assertEquals("11,21", System.out.println(decimalFormater.format(11.219)) );
-//	assertEquals("+ 1 234 556.21", decimalFormater.format(1234556.21));
-//	assertEquals("- 1 234 556.21", decimalFormater.format(-1234556.21);
-	}
-
-    private static DecimalFormat decimalFormater = new DecimalFormat("### ### ##0.00");{
+    private static DecimalFormat decimalFormater = new DecimalFormat("0.00");{
 		decimalFormater.setNegativePrefix("- ");
 		decimalFormater.setPositivePrefix("+ ");
+		decimalFormater.setGroupingSize(3);
+		decimalFormater.setGroupingUsed(true);
+		
+		decimalFormater.setDecimalFormatSymbols(DecimalFormatSymbols.getInstance(Locale.FRANCE));
+
 	}
 	private static SimpleDateFormat dateformatter = new SimpleDateFormat("dd/MM/yyyy");
 
